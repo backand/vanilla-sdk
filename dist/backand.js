6bb2c1ae0660930da876af56701bfdd9b6e1a8cc
Ran Cohen <rannn505@outlook.com>
2017-02-09 18:10:39 +0200

    build 1.1.0

--- dist/backand.js
+++ dist/backand.js
@@ -1,6 +1,6 @@
 /*********************************************************
  * @backand/vanilla-sdk - Backand SDK for JavaScript
- * @version v1.0.9
+ * @version v1.1.0
  * @link https://github.com/backand/vanilla-sdk#readme
  * @copyright Copyright (c) 2017 Backand https://www.backand.com/
  * @license MIT (http://www.opensource.org/licenses/mit-license.php)
