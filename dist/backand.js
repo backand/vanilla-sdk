e848c7c8c9aa7c58bbbe775917cb0aa328424747
relly <relly@backand.com>
2017-07-02 10:33:50 +0300

    Release version 1.2.7

--- dist/backand.js
+++ dist/backand.js
@@ -1,6 +1,6 @@
 /*********************************************************
  * @backand/vanilla-sdk - Backand SDK for JavaScript
- * @version v1.2.6
+ * @version v1.2.7
  * @link https://github.com/backand/vanilla-sdk#readme
  * @copyright Copyright (c) 2017 Backand https://www.backand.com/
  * @license MIT (http://www.opensource.org/licenses/mit-license.php)
